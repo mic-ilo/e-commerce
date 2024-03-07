c204c5f937e351bd842315105534092e3b615bff
mic-ilo <michelaquino1003@gmail.com>
2024-03-07 14:22:32 +0800

    setup test page
    create test page
    add test content
    add button

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>Test page
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus et cumque qui. Quis, natus assumenda dolores deserunt tempore atque sint quae mollitia, molestias inventore, eius iste! Magnam molestiae aliquid, placeat dignissimos et ipsam quae nobis earum doloribus rem obcaecati consequatur esse dolore rerum facere non qui, reiciendis omnis dolorum.</p>
+        <button className='bg-red-200 px-5 py-2 rounded-lg mt-5'>Click me!!</button>
+    </div>
+  )
+}
